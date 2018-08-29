8c9c27ab11e35939ca51a15e72f62171b00fd4e1
uparikh <urvish.parikh@soprabanking.com>
2018-08-29 13:59:39 +0200

    commit

---
+++ src/main/java/com/controller/CallmeController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CallmeController {
+
+
+    private static String name = "call-me";
+
+    private static String version = "v1";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CallmeController.class);
+
+    @RequestMapping("/ping")
+    public String ping() {
+        LOGGER.info("Ping: name={}, version={}", name, version);
+        return name + ":" + version;
+    }
+
+}
